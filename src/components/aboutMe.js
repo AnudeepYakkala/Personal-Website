0f9cfbe73523ecf446cbc341c067006d8c38cab0
Anudeep Yakkala <ayakkala@purdue.edu>
2020-10-27 00:59:13 -0700

    Bug fixes and update information.

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -4,64 +4,150 @@ import { Link } from 'react-router-dom';
 import './aboutMe.css';
 
 export default class aboutMe extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isDesktop: false
+        };
+
+        this.updatePredicate = this.updatePredicate.bind(this);
+    }
+
+    componentDidMount() {
+        this.updatePredicate();
+        window.addEventListener("resize", this.updatePredicate);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updatePredicate);
+    }
+
+    updatePredicate() {
+        this.setState({isDesktop: window.innerWidth > 1200});
+    }
+
     render() {
+        const isDesktop = this.state.isDesktop;
+
         return (
-            <div className="color">
-                <h1 class="ProjectTitle3">ABOUT ME</h1>
-                <div className="About">
-                    > Anudeep.education
-                    <br></br>
-                    <font class="Output"> —> “Sophomore at Purdue University”</font>
-                    <br></br>
-                    <br></br>
-                    > Anudeep.major
-                    <br></br>
-                    <font class="Output">—> “Computer Science”</font>
-                    <br></br>
-                    <br></br>
-                    > Anudeep.interests
-                    <br></br>
-                    <font class="Output">—> [“Coding”, “Basketball”, “Nature”, “Hiking”, “Gaming”, “Music”]</font>
-                    <br></br>
-                    <br></br>
-                    > Anudeep.techicalSkills
-                    <br></br>
-                    <font class="Output">—> [“Java”, “C”, “Python”, “HTML/CSS”, “Data Structures and Algorithms”, “Machine Learning”]</font>
-                    <br></br>
-                    <br></br>
-                    > Anudeep.latestWorkExperience
-                    <br></br>
-                    <font class="Output">—> Company: “Google”
-                    <br></br>&nbsp;&nbsp;&nbsp;&nbsp; Role: “STEP Intern”
-                    <br></br>&nbsp;&nbsp;&nbsp;&nbsp; When: “Present”
-                    <br></br>&nbsp;&nbsp;&nbsp;&nbsp; Where: “San Francisco Bay Area”</font>
-                    <br></br>
-                    <br></br>
-                    > Anudeep.resume
-                    <br></br>
-                    <font class="Output">—></font> <Link to={{pathname: '/resume'}}><font class="Link">“resume.pdf”</font>
-                    </Link>
-                    <br></br>
-                    <br></br>
-                    > Anudeep.email
-                    <br></br>
-                    <font class="Output">—></font> 
-                    <a href="mailto:anudeep.yakkala27@gmail.com"><font class="Link">“anudeep.yakkala27@gmail.com”</font></a>
-                    <br></br>
-                    <br></br>
-                    > Anudeep.github
-                    <br></br>
-                    <font class="Output">—></font>
-                    <a href="http://www.github.com/AnudeepYakkala"><font class="Link">“github.com/AnudeepYakkala”</font></a>
-                    <br></br>
-                    <br></br>
-                    > Anudeep.linkedIn
-                    <br></br>
-                    <font class="Output">—></font>
-                    <a href="http://linkedin.com/in/ayakkala/"><font class="Link">“linkedin.com/in/ayakkala”</font></a>
-                </div>
-                <div className="footer"></div>
+            <div>
+                {isDesktop ? (
+                    <div className="color">
+                        <h1 class="ProjectTitle3">ABOUT ME</h1>
+                        <div className="About">
+                            {">"} Anudeep.education
+                            <br></br>
+                            <font class="Output"> —{">"} Junior at Purdue University”</font>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.major
+                            <br></br>
+                            <font class="Output">—{">"} “Computer Science”</font>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.interests
+                            <br></br>
+                            <font class="Output">—{">"} [“Coding”, “Basketball”, “Nature”, “Hiking”, “Gaming”, “Music”]</font>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.techicalSkills
+                            <br></br>
+                            <font class="Output">—{">"} [“Java”, “JavaScript”, “Python”, “Bash Scripting", “Node.js”, “React", "SQL", "Git"]</font>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.latestWorkExperience
+                            <br></br>
+                            <font class="Output">—{">"} Company: “Google”
+                            <br></br>&nbsp;&nbsp;&nbsp;&nbsp; Role: “STEP Intern”
+                            <br></br>&nbsp;&nbsp;&nbsp;&nbsp; When: “Summer 2020”
+                            <br></br>&nbsp;&nbsp;&nbsp;&nbsp; Where: “San Francisco Bay Area”</font>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.resume
+                            <br></br>
+                            <font class="Output">—{">"}</font> <Link to={{pathname: '/resume'}}><font class="Link">“resume.pdf”</font>
+                            </Link>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.email
+                            <br></br>
+                            <font class="Output">—{">"}</font> 
+                            <a href="mailto:anudeep.yakkala27@gmail.com"><font class="Link">“anudeep.yakkala27@gmail.com”</font></a>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.github
+                            <br></br>
+                            <font class="Output">—{">"}</font>
+                            <a href="http://www.github.com/AnudeepYakkala"><font class="Link">“github.com/AnudeepYakkala”</font></a>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.linkedIn
+                            <br></br>
+                            <font class="Output">—{">"}</font>
+                            <a href="http://linkedin.com/in/ayakkala/"><font class="Link">“linkedin.com/in/ayakkala”</font></a>
+                        </div>
+                        <div className="footer"></div>
+                    </div>
+                ) : (
+                    <div className="color">
+                        <h1 class="ProjectTitle3">ABOUT ME</h1>
+                        <div className="About-mobile">
+                            {">"} Anudeep.education
+                            <br></br>
+                            <font class="Output"> —{">"} Junior at Purdue University”</font>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.major
+                            <br></br>
+                            <font class="Output">—{">"} “Computer Science”</font>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.interests
+                            <br></br>
+                            <font class="Output">—{">"} [“Coding”, “Basketball”, “Nature”, “Hiking”, “Gaming”, “Music”]</font>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.techicalSkills
+                            <br></br>
+                            <font class="Output">—{">"} [“Java”, “JavaScript”, “Python”, “Bash Scripting", “Node.js”, “React", "SQL", "Git"]</font>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.latestWorkExperience
+                            <br></br>
+                            <font class="Output">—{">"} Company: “Google”
+                            <br></br>&nbsp;&nbsp;&nbsp;&nbsp; Role: “STEP Intern”
+                            <br></br>&nbsp;&nbsp;&nbsp;&nbsp; When: "Summer 2020"
+                            <br></br>&nbsp;&nbsp;&nbsp;&nbsp; Where: “San Francisco Bay Area”</font>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.resume
+                            <br></br>
+                            <font class="Output">—{">"}</font> <Link to={{pathname: '/resume'}}><font class="Link">“resume.pdf”</font>
+                            </Link>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.email
+                            <br></br>
+                            <font class="Output">—{">"}</font> 
+                            <a href="mailto:anudeep.yakkala27@gmail.com"><font class="Link">“anudeep.yakkala27@gmail.com”</font></a>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.github
+                            <br></br>
+                            <font class="Output">—{">"}</font>
+                            <a href="http://www.github.com/AnudeepYakkala"><font class="Link">“github.com/AnudeepYakkala”</font></a>
+                            <br></br>
+                            <br></br>
+                            {">"} Anudeep.linkedIn
+                            <br></br>
+                            <font class="Output">—{">"}</font>
+                            <a href="http://linkedin.com/in/ayakkala/"><font class="Link">“linkedin.com/in/ayakkala”</font></a>
+                        </div>
+                        <div className="footer"></div>
+                    </div>
+                )}
             </div>
+            
         )
     }
 }
--- src/components/image.js
+++ src/components/image.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './image.css';
+import LaptopImage from './images/website-image.png';
 
 export default class Image extends React.Component {
     render() {
@@ -8,11 +9,7 @@ export default class Image extends React.Component {
             <div>
                 <div className="image-header"></div>
                 <div class="image-container">
-                    <div class="container">
-                    <div class="row">
-                        <img class='img-fluid w-100' src="https://lh3.googleusercontent.com/6BpGI73M_xva6If_PPFC6DP7seUa_e8W7_JaUUZ-6-7hmqfb2VtFsYj4D7TVTRSnRpBQUVt9gNgpTupSB_Wty50RvtCQkFMPoN78Wt4b_iBKQFAx7uoVgEsWg0EyyfSwNJJvXwmvhg=w847" alt="" />
-                    </div>
-                    </div>
+                        <img className="image-size" src={LaptopImage} />
                 </div>
             </div>
         )
